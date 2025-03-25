dffa9764c26b523004d240b82e46c2944d5282d0
Billy Rugamba <61375439+billanx@users.noreply.github.com>
2025-03-25 13:16:32 +0200

    Feature: Get Invoice by invoice id

--- src/main/java/com/ishyiga/repo/InvoiceRepository.java
+++ src/main/java/com/ishyiga/repo/InvoiceRepository.java
@@ -3,12 +3,13 @@ package com.ishyiga.repo;
 import com.ishyiga.entities.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
-    @Query("select i from Invoice i where i.idInvoice = ?")
-    Optional<Invoice> findByIdInvoice(Integer idInvoice);
+    @Query("select i from Invoice i where i.idInvoice = :idInvoice")
+    Optional<Invoice> findByIdInvoice(@Param("idInvoice") Integer idInvoice);
 }
